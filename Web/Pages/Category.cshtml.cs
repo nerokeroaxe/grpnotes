3eaa3d63c59985c31886baa8a5b336d9ed35e718
nerokeroaxe <danilabal1119@gmail.com>
2024-11-18 12:35:11 +0400

    fix: страницы категорий теперь работают как надо

--- Web/Pages/Category.cshtml.cs
+++ Web/Pages/Category.cshtml.cs
@@ -3,6 +3,7 @@ using Contracts.Services;
 using Contracts.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Web.Pages;
 
@@ -10,10 +11,10 @@ public class CategoryModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
     private readonly ICategoryService _categoryService;
-    public List<CategoryView> Categories { get; set; }
+    public List<CategoryView> Categories { get; set; } = new();
     
     [BindProperty]
-    public CategoryDto Category { get; set; }
+    public CategoryDto Category { get; set; } = new();
     
     public CategoryModel(ILogger<IndexModel> logger, ICategoryService categoryService)
     {
@@ -21,22 +22,47 @@ public class CategoryModel : PageModel
         _categoryService = categoryService;
     }
 
-    public async Task OnGet()
-    {
-        Categories = (await _categoryService.GetAll()).ToList();
+    public async Task<IActionResult> OnGetAsync()
+    {   
+        try
+        {
+            Categories = (await _categoryService.GetAll()).ToList();
+            _logger.LogInformation("Categories: {Categories}", Categories.Count);
+        } 
+        catch(Exception e)
+        {
+            _logger.LogError("Error on get categories: " + e.Message);
+        }
+        return Page();
     }
 
     public async Task<IActionResult> OnPost()
     {
-        await _categoryService.Create(Category);
+        _logger.LogInformation("Create category on page: {Category}", Category.Name);
+        try 
+        {
+            await _categoryService.Create(Category);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError("Error on create category: " + e.Message);
+        }
         
-        return Page();
+        return RedirectToPage("Category");
     }
 
     public async Task<IActionResult> OnPostRemove(Guid id)
     {
-        await _categoryService.Remove(id);
-        
-        return Page();
+        _logger.LogInformation("Remove category on page id: {id}", id);
+        try 
+        {
+            await _categoryService.Remove(id);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError("Error on remove category: " + e.Message);
+        }
+
+        return RedirectToPage("Category");
     }
 }
